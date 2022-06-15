5e13564d3e0f756a58af8731a13b2b1a26b0af00
Anibal Oziel <anibaloziel.alvarado@gmail.com>
2022-06-15 06:38:15 -0500

    fisrt commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }, 
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader'}
+                ]
+            },
+            {
+                test:
+            }          
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },        
+            compress: true,           
+            open: true,
+            port: 3006,         
+    }
+} 
